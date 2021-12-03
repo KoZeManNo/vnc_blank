6bc2ad97f17c9724b2a72362dae385d652f0306d
Kevin <kevin@kozemanno.dev>
2021-12-03 09:38:44 +0100

    Add files via upload

---
+++ _blank.js
@@ -0,0 +1,8 @@
+document.addEventListener("transitionstart", () => {
+    let consoleButton = document.getElementById("consoleButton");
+
+    if(consoleButton !== null) {
+        consoleButton.removeAttribute("onClick");
+        consoleButton.setAttribute("target", "_blank");
+    }
+})
